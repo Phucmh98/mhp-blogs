f12525680706cc3b72f7fac8133298b82f919e8f
Phuc <mhphuc.98@gmail.com>
2025-05-25 15:15:52 +0700

    fix[remove useState not use]

--- src/components/commons/view/threejs/threejs-scene.tsx
+++ src/components/commons/view/threejs/threejs-scene.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
