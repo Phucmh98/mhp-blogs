eeac8dcacb1a91d07be8cbf27e431daefbad4cbb
Phuc <mhphuc.98@gmail.com>
2025-06-04 14:43:13 +0700

    feat[Update Detail Page Project]

--- src/app/(main)/lib/select-project.ts
+++ src/app/(main)/lib/select-project.ts
@@ -1,8 +1,8 @@
 
 export interface Project {
+    id: string
     name: string;
     description: string;
-    url: string;
     content?: string;
     image: string;
     urlGithub?: string;
@@ -11,44 +11,93 @@ export interface Project {
     status?: "completed" | "in-progress" | "upcoming";
     backgroundColor: string
     startDate?: string;
+    contentDetail?: ProjectDetail[];
+    role?: string;
+    client?: string;
+}
+
+export interface ProjectDetail {
+    content?: string;
+    typeContent?: "title" | "text" | "image" | "video" | "list";
+    contentList?: string[];
 }
 
 export const selectProjects: Project[] = [
     {
+        id: "googleDocsClone",
         name: "Google Docs Clone",
         description: "A clone of Google Docs",
-        url: "/",
         image: "/image/projects/banner-google-docs-clone.png",
         urlGithub: "https://github.com/",
         urlDemo: "/",
         type: "web",
         status: "completed",
         backgroundColor: "bg-green-200",
-        startDate:"2025"
+        startDate: "2025"
     },
     {
+        id: "messengerClone",
         name: "Messenger Clone",
         description: "A clone of Messenger",
-        url: "/",
         image: "/image/projects/banner-messenger-clone.png",
         urlGithub: "https://github.com/",
         urlDemo: "/",
         type: "web",
         status: "completed",
         backgroundColor: "bg-blue-200",
-        startDate:"2025"
+        startDate: "2025"
     },
     {
+        id: "mhpCinema",
         name: "MHP Cinema",
         description: "Front End course completion project",
-        url: "/",
-        image: "/image/projects/banner-mhp-movie.png",
+        image: "/image/projects/carousel_mhp.png",
         urlGithub: "https://github.com/",
         urlDemo: "/",
         type: "web",
         status: "completed",
         backgroundColor: "bg-pink-200",
-        startDate:"2023"
+        startDate: "2023",
+        client: "Project Personal",
+        role: "Frontend Developer",
+        content: "A personal project developed to learn and improve Frontend skills. This web application allows users to book movie tickets online, select cinemas, choose seats, and make payments quickly. Focused on delivering a responsive, user-friendly interface and smooth experience across all devices.",
+        contentDetail: [
+            {
+                typeContent: "title",
+                content: "Overview"
+            },
+            {
+                typeContent: "text",
+                content: "MHPcinemas is a movie ticket booking website developed by me to training exercises. The project is not only a personal endeavor but also an opportunity for me to apply and expand my knowledge of the technologies I have learned."
+            },
+            {
+                typeContent: "title",
+                content: "Sceen Image"
+            },
+            {
+                typeContent: "image",
+                content: "/image/projects/mhp_ladingpage.png"
+            },
+            {
+                typeContent: "title",
+                content: "Tech Stack"
+            },
+            {
+                typeContent: "list",
+                contentList: ["ReactJs","Tailwind CSS", "Ant Design","Axios"]
+            },
+            {
+                typeContent: "title",
+                content: "Features"
+            },
+            {
+                typeContent: "list",
+                contentList: ["Login/Register", "Home page: Shows movie list (include: Show All, Now Showing, Comming Soon), Find movies, Popup Video", "Detail Movie page: Show information movie, show cinemas", "Detail Cinemas page: Show seats and choose seats, show detail tickets", "Cinemas page: Show detail cinemas and movies, show information cinemas include: name, location, showtime", "User page: CRUD user, find user","User movie: CRUD movie, find movie"]
+            },
+
+        ]
+
+
     },
 
 ]
--- src/app/(main)/my-projects/components/collection-project.tsx
+++ src/app/(main)/my-projects/components/collection-project.tsx
@@ -1,33 +1,33 @@
-'use client';
+"use client";
 import BlurImage from "@/components/commons/image/blur-image";
 import SpotlightCard from "@/components/commons/reactbits/spotlight-card";
 import { Project, selectProjects } from "../../lib/select-project";
 import { useEffect } from "react";
+import Link from "next/link";
 
 const CollectionProject = () => {
-
   useEffect(() => {
-    const cards = document.querySelectorAll('.card');
+    const cards = document.querySelectorAll(".card");
 
     function handleMouseEnter(e: Event) {
-      cards.forEach(c => {
-        if (c !== e.currentTarget) c.classList.add('dimmed');
+      cards.forEach((c) => {
+        if (c !== e.currentTarget) c.classList.add("dimmed");
       });
     }
 
     function handleMouseLeave() {
-      cards.forEach(c => c.classList.remove('dimmed'));
+      cards.forEach((c) => c.classList.remove("dimmed"));
     }
 
-    cards.forEach(card => {
-      card.addEventListener('mouseenter', handleMouseEnter);
-      card.addEventListener('mouseleave', handleMouseLeave);
+    cards.forEach((card) => {
+      card.addEventListener("mouseenter", handleMouseEnter);
+      card.addEventListener("mouseleave", handleMouseLeave);
     });
 
     return () => {
-      cards.forEach(card => {
-        card.removeEventListener('mouseenter', handleMouseEnter);
-        card.removeEventListener('mouseleave', handleMouseLeave);
+      cards.forEach((card) => {
+        card.removeEventListener("mouseenter", handleMouseEnter);
+        card.removeEventListener("mouseleave", handleMouseLeave);
       });
     };
   }, []);
@@ -56,27 +56,29 @@ export default CollectionProject;
 const CardProject = (project: Project) => {
   return (
     <div className="card cursor-pointer transition-all duration-300 text-gray-500 dark:text-gray-300">
-      <SpotlightCard
-        className={`group w-full cursor-pointer rounded-2xl p-6 md:p-5 lg:p-8 ${project.backgroundColor}`}
-        // className={`group w-full cursor-pointer rounded-2xl p-8 bg-orange-200`}
+      <Link href={`/my-projects/${project.id}`}>
+        <SpotlightCard
+          className={`group w-full cursor-pointer rounded-2xl p-6 md:p-5 lg:p-8 ${project.backgroundColor}`}
+          // className={`group w-full cursor-pointer rounded-2xl p-8 bg-orange-200`}
 
-        spotlightColor="rgba(254, 154, 0, 0.3)"
-      >
-        <BlurImage
-          width={1280}
-          height={832}
-          src={project.image}
-          alt={project.name}
-          className="h-[270px] md:h-[210px] lg:h-[250px] border-3  border-gray-200 rounded-2xl shadow-xl/30 transition-transform duration-300 group-hover:scale-103"
-        />
-      </SpotlightCard>
-      <div className="flex w-full justify-between items-center px-2">
-        <div className="mt-2">
-          <h3 className="text-2xl font-semibold">{project.name}</h3>
-          <p className="mt-0.5 dark:text-gray-400">{project.description}</p>
+          spotlightColor="rgba(254, 154, 0, 0.3)"
+        >
+          <BlurImage
+            width={1280}
+            height={832}
+            src={project.image}
+            alt={project.name}
+            className="h-[270px] md:h-[210px] lg:h-[250px] border-3  border-gray-200 rounded-2xl shadow-xl/30 transition-transform duration-300 group-hover:scale-103"
+          />
+        </SpotlightCard>
+        <div className="flex w-full justify-between items-center px-2">
+          <div className="mt-2">
+            <h3 className="text-2xl font-semibold">{project.name}</h3>
+            <p className="mt-0.5 dark:text-gray-400">{project.description}</p>
+          </div>
+          <span>{project.startDate}</span>
         </div>
-        <span>{project.startDate}</span>
-      </div>
+      </Link>
     </div>
   );
 };
